9ead09831bf76f2ef502e292620d687ad28422b3
hrigu <chmuehlethaler@gmail.com>
2015-05-14 20:35:54 +0200

    apache erweitert

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,2 +1,13 @@
 # puppet module install puppetlabs-apache
-class { 'apache':  }
+# mit sudo: sudo puppet apply manifests/apache.pp
+class { 'apache':
+  default_vhost => false,
+  default_mods        => false,
+  default_confd_files => false,
+}
+
+apache::vhost { 'first.example.com':
+  port    => '3000',
+  docroot => '/var/www/first',
+}
+
